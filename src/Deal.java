b510fe38bb9c13048973ea6e8546a15d432b0296
Janar <jenku18@gmail.com>
2016-10-06 13:43:01 +0300

    first commit

---
+++ src/Deal.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+
+public class Deal {
+	public static void main(String args[]) {
+		List<List<Card>> listOfPlayersCards = new ArrayList<>();
+
+		int numHands = 2;
+		int cardsPerHand = 26;
+		List<Card> deck = Card.newDeck();
+		// System.out.println(deck.get(14).getCardValue());
+		Collections.shuffle(deck);
+		for (int i = 0; i < numHands; i++) {
+			// System.out.println(deal(deck, cardsPerHand));
+			listOfPlayersCards.add(deal(deck, cardsPerHand));
+		}
+
+		System.out.println(listOfPlayersCards);
+	}
+
+	public static ArrayList<Card> deal(List<Card> deck, int n) {
+		int deckSize = deck.size();
+		List<Card> handView = deck.subList(deckSize - n, deckSize);
+		ArrayList<Card> hand = new ArrayList<Card>(handView);
+		handView.clear();
+		return hand;
+	}
+}
