27062439fb4aadd8ff1085814837ac57848b31b5
Janar <jenku18@gmail.com>
2016-10-18 13:34:26 +0300

    test card rank differences

--- src/cards/GameTests.java
+++ src/cards/GameTests.java
@@ -10,9 +10,16 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class GameTests {
 
+	Card random = new Card(Rank.SIX, Suit.SPADES);
 	@Test
 	public void isSameColor(){
 		Card card = new Card(Rank.FIVE, Suit.SPADES);
 		assertThat(card.isRed() ,is(false));
+	}
+	
+	@Test
+	public void rankDiffIsLessThanThree(){
+		Card card = new Card(Rank.EIGHT, Suit.DIAMONDS);
+		assertThat(card.rankDif(random) ,is(true));
 	}
 }
